42606dbc7e2435fe5d447429f3829ad432748073
atharva007-cmd <atharvamarathe007@gmail.com>
2020-04-02 11:41:42 +0530

    Added Comments

--- SY-Assignments/OS/Scheduling Algorithms/FCFS/FCFS.c
+++ SY-Assignments/OS/Scheduling Algorithms/FCFS/FCFS.c
@@ -1,3 +1,27 @@
+/**
+ * - FirstCome First Serve - Scheduling Algorithm
+ * - Author: Atharva Marathe
+ * - Date: 01/03/2020
+ * 
+ * - This is a simple user-friendly program that demonstrates 
+ *   FirstCome First Serve Algorithm through dummy processes.
+ * 
+ * - Compile: gcc -Wall -Werror -std=c99 FCFS.c -o FCFS
+ * - Run: ./FCFS
+ * 
+ * - Input:
+ *   - Manual Input for Process-ID (ID), Arrival-Time (AT), Burst-Time (BT)
+ * 
+ * - Output:
+ *   - Chart displaying scheduling through FCFS manner
+ *   - Calculation of Completion-Time (CT), Turn-Around-Time (TAT)
+ *   - Calculation of Average CT, TAT
+ *  
+ * - Limits/Known bugs: 
+ *   -  
+ * 
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,59 +42,65 @@ int main(int args, char* argv[])
     int i=0, num=0, ID, AT, BT, totalTAT=0, totalCT=0;
     temp=head;
     
+    // Getting Number of Processes
     printf("Enter number of processes you want to schedule: ");
     scanf("%d", &num);
 
+    // Creating ordered-LL of Processes
     for (i=0; i<num; i++) createNode(&head);
+    
+    // Calculating CT, Total CT
     for (temp=head; temp!=NULL; temp=temp->next)
     {
         if (temp->prev == NULL)
-        {
             (temp->CT)=(temp->BT)+(temp->AT);
-        }
         else 
         {
             if (((temp->prev)->CT) > temp->AT)
-            {
                 temp->CT=(temp->prev)->CT+temp->BT;
-            }
             else
-            {
-                (temp->CT)=(temp->BT)+(temp->AT);
-            }                        
+                (temp->CT)=(temp->BT)+(temp->AT);                        
         } 
         totalCT+=temp->CT;       
     }
 
+    // Calculating TAT, Total TAT
     for (temp=head; temp!=NULL; temp=temp->next)
     {
         temp->TAT=temp->CT-temp->AT;
         totalTAT+=temp->TAT;
     }
 
+    // Displaying the processes in FCFS scheduled manner
     printf("\nProcess-ID\tAT\tBT\tCT\tTAT\n");
     for (temp=head; temp!=NULL; temp=temp->next) printf("    P%d\t\t%d\t%d\t%d\t%d\n", temp->ID, temp->AT, temp->BT, temp->CT, temp->TAT);
 
+    // Displaying Average TAT, CT
     printf("\nAverage CT: %d\nAverage TAT: %d\n", (totalCT/num), (totalTAT/num));
   
+    // Freeing the ordered-LL
     deleteNode(head); 
+    
     printf("\nSuccessfully Executed\n"); 
     return 0;
 }
 
+
+// Create ordered-LL routine
 void createNode (struct node** head)
 {
     struct node *newNode, *temp;
+    
+    // Initializing new Process
     newNode = (struct node*) malloc (sizeof(struct node));
     printf("Enter ID, AT, BT: ");
     scanf("%d%d%d", &newNode->ID, &newNode->AT, &newNode->BT);
     newNode->CT=newNode->TAT=0;
     newNode->next = newNode->prev =  NULL;
 
+    // Appending process nodes in appropriate order
     if (*head==NULL)
-    {
         (*head)=newNode;
-    } 
     else
     {
         if ((*head)->AT > newNode->AT)
@@ -83,9 +113,7 @@ void createNode (struct node** head)
         {
             for (temp=(*head); temp->next!=NULL && ((temp->next)->AT < newNode->AT); temp=temp->next);
             if (temp->next==NULL)
-            {
                 temp->next=newNode;
-            }
             else 
             {
                 newNode->next=temp->next;
@@ -98,6 +126,8 @@ void createNode (struct node** head)
     return;
 }
 
+
+// Delete Routine
 void deleteNode (struct node* head)
 {
     struct node *temp, *temp1;
