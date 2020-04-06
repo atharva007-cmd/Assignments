102048562be5f85e41399092748eabe0cd1cefbd
atharva007-cmd <atharvamarathe007@gmail.com>
2020-04-06 19:59:24 +0530

    C Program

---
+++ SY-Assignments/TOC/Assignment 2/myScanner.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+extern int yylex();
+
+int main (void)
+{
+    int token;
+    token = yylex();
+    while (token)
+    {
+        switch (token)
+        {
+        case 1:
+            printf(" COMMENT\n");
+            break;
+
+        case 2:
+            printf(" ACCESS_SPECIFIER ");
+            break;
+
+        case 3:
+            printf(" SEPERATOR ");
+            break;
+
+        case 4:
+            printf(" KEY_WORD ");
+            break;
+        }
+        token = yylex();
+    }
+    printf("\n");
+    return 0;
+}
